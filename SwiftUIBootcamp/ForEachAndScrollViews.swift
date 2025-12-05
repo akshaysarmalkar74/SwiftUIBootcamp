0b62034a5ccc1d26ef21a7d7c6511e1e6581a4e2
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2025-12-05 19:36:33 +0530

    Add ForEach & ScrollViews

---
+++ SwiftUIBootcamp/ForEachAndScrollViews.swift
@@ -0,0 +1,18 @@
+//
+//  ForEachAndScrollViews.swift
+//  SwiftUIBootcamp
+//
+//  Created by Akshay Sarmalkar on 05/12/25.
+//
+
+import SwiftUI
+
+struct ForEachAndScrollViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForEachAndScrollViews()
+}
