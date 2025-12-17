24e120e49e4ada5e5884c9a319d616911917b6bb
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2025-12-17 23:22:23 +0530

    Add Picker Bootcamp

---
+++ SwiftUIBootcamp/PickerBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  PickerBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Akshay Sarmalkar on 16/12/25.
+//
+
+import SwiftUI
+
+struct PickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerBootcamp()
+}
