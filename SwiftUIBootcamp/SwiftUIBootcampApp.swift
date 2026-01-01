57d5ef14671dc9a0a5ee17b7a839f27af6a03f87
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2026-01-01 18:58:51 +0530

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Akshay Sarmalkar on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TextBootcamp()
+        }
+    }
+}
