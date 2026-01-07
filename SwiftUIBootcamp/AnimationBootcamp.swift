66beea38586ad12f7d402ec790bc52091ebfa1fe
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2026-01-07 20:25:48 +0530

    Add Animation Bootcamp

---
+++ SwiftUIBootcamp/AnimationBootcamp.swift
@@ -0,0 +1,53 @@
+//
+//  AnimationBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Akshay Sarmalkar on 07/01/26.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    @State private var isAnimating: Bool = true
+    
+    var body: some View {
+        VStack {
+            Text("Animation Bootcamp")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Button {
+                withAnimation {
+                    isAnimating.toggle()
+                }
+                
+            } label: {
+                Text("Toggle Animate")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color.white)
+                    .padding()
+                    .background(
+                        Color.accent
+                            .cornerRadius(8)
+                    )
+            }
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: isAnimating ? 50 : 20)
+                .fill(isAnimating ? Color.orange : Color.yellow)
+                .frame(
+                    width: isAnimating ? 100 : 300,
+                    height: isAnimating ? 100 : 200
+                )
+                .offset(y: isAnimating ? 250 : 0)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AnimationBootcamp()
+}
